37e12a36c0c8758e5ee72f6553f7620267a58060
KietLe261003 <letuankiet26102003kl@gmail.com>
2024-12-11 22:48:14 +0700

    detail course admin

--- src/Page/AdminPage/EditCourse/CourseDetailPage.tsx
+++ src/Page/AdminPage/EditCourse/CourseDetailPage.tsx
@@ -1,9 +1,26 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import CourseRoadMap from "./CourseRoadMap";
+import { useEffect, useState } from "react";
+import { ApiResponseLesson, Lesson } from "../../../Type/Lesson";
+import { lessonService } from "../../../Services/LessonService";
 
 const CourseDetailPage = () => {
-
+  const {id}=useParams();
+  const [listLesson,setListLesson]=useState<Lesson[]>([]);
   const navigate = useNavigate();
+  const getAllLesson=async()=>{
+    try {
+      const courseId: number = Number(id);
+      const res: ApiResponseLesson = await lessonService.getAllLessonByCourseId(courseId);
+      setListLesson(res.content);
+    } catch (error) {
+      console.log(error);
+      alert("Lấy thất bại");
+    }
+  }
+  useEffect(()=>{
+    getAllLesson();
+  },[])
   return (
     <div className='p-6 bg-gray-100'>
       <table className='w-full bg-white shadow-md rounded-lg'>
@@ -36,8 +53,8 @@ const CourseDetailPage = () => {
       </table>
       <div className='flex mt-6'>
         <div className='w-1/3 bg-white shadow-md rounded-lg p-4 text-center'>
-          <img src='' alt='User' className='w-20 h-20 mx-auto rounded-full' />
-          <h3 className='mt-4 text-lg font-semibold'>admin</h3>
+          <img src='https://scontent.fsgn16-1.fna.fbcdn.net/v/t39.30808-1/283579103_1332300267263416_6711789697801402891_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFyI-tHUZuieaa_eArPuPTC9TYAOO7dDBH1NgA47t0MEQkKARdBL-iUlta5SoMNOrH4MOsZxkbFq-YUPi3jEl5x&_nc_ohc=7vkwkeDBryMQ7kNvgFcpWnf&_nc_zt=24&_nc_ht=scontent.fsgn16-1.fna&_nc_gid=AbW0zcQyUnomn8KVGSYE9iY&oh=00_AYBKrHW3nRXVWBOHSk5HTcUt-zTpuTh4F3X4762iS8fFnQ&oe=675F5FB3' alt='User' className='w-20 h-20 mx-auto rounded-full' />
+          <h3 className='mt-4 text-lg font-semibold text-black'>Quốc Trung</h3>
           <div className='flex justify-center mt-2 space-x-4'>
             <a href='#' className='text-blue-500'>
               Twitter
@@ -59,15 +76,18 @@ const CourseDetailPage = () => {
           ></textarea>
         </div>
       </div>
-
-      <div className='mt-6'>
+      <div className=' my-6'>
         <button
         onClick={() => {navigate('/admin/chapterform')}}
         className='bg-blue-500 text-white py-2 px-4 rounded'>
           Tạo Chương
         </button>
       </div>
-      <CourseRoadMap></CourseRoadMap>
+      {
+        listLesson.map((item,index)=>(
+          <CourseRoadMap key={index} idRoadMap={item.lessonId} nameRoadMap={item.name} descriptionRoadMap={item.content}/>
+        ))
+      }
     </div>
   );
 };
--- src/Page/AdminPage/EditCourse/CourseRoadMap.tsx
+++ src/Page/AdminPage/EditCourse/CourseRoadMap.tsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IconArrowRight } from "../../../Common/Icon/Icon";
-
-const CourseRoadMap=()=> {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChalkboard } from '@fortawesome/free-solid-svg-icons';
+import { Document } from "../../../Type/Document/Document";
+import { documentService } from "../../../Services/DocumentService";
+import { DocumentResponseGetAll } from "../../../Type/Document/DocumentResponse";
+interface CourseRoadMapProps{
+  idRoadMap: number,
+  nameRoadMap: string,
+  descriptionRoadMap: string,
+}
+const CourseRoadMap:React.FC<CourseRoadMapProps>=({idRoadMap,nameRoadMap,descriptionRoadMap})=> {
     const [isDropdownOpen, setDropdownOpen] = useState<boolean>(false);
+    const [listDocument, setListDocument] = useState<Document[]>([]);
     const toggleDropdown = () => {
         setDropdownOpen(!isDropdownOpen);
     };
+    const getAllDocumentByLesson= async ()=>{
+      const res:DocumentResponseGetAll=await documentService.getDocumentByLesson(idRoadMap);
+      setListDocument(res.content);
+    }
+    const update = ()=>{
+      alert("Haha");
+    }
+    useEffect(()=>{
+      getAllDocumentByLesson();
+    },[idRoadMap])
     return (
       <div className='flex flex-col rounde-lg mb-2'>
         <button
@@ -13,19 +33,19 @@ const CourseRoadMap=()=> {
           data-dropdown-toggle='dropdown'
           onClick={toggleDropdown}
           type='button'
-          className='flex items-center justify-between w-full p-2 lg:rounded-full md:rounded-full hover:bg-gray-100 cursor-pointer border-2 rounded-lg'
+          className='flex items-center justify-between w-full bg-white p-5 lg:rounded-full md:rounded-full  cursor-pointer border-2'
         >
-          <div className='lg:flex md:flex items-center'>
-            {/* <FontAwesomeIcon className="h-12 w-12 lg:mb-0 md:mb-0 m-3" icon={faChalkboard} /> */}
-            <div className='flex flex-col'>
+          <div className='lg:flex md:flex flex items-center'>
+            <FontAwesomeIcon className="h-12 w-12 lg:mb-0 md:mb-0 mx-3" icon={faChalkboard} />
+            <div>
               <div className='flex justify-start text-xl mb-2 leading-3 text-gray-700 font-bold w-full'>
-                Chương 1
+                {nameRoadMap}
               </div>
-              <div className='text-sm text-gray-600 w-full te'>Chương 1</div>
+              <div className='text-sm text-gray-600 w-full'>{descriptionRoadMap}</div>
             </div>
           </div>
           <div className='flex flex-row mr-4 justify-center items-center gap-3'>
-            <button className='bg-yellow-500 text-white py-1 px-4 rounded'>
+            <button onClick={update} className='bg-yellow-500 text-white py-1 px-4 rounded'>
               Sửa
             </button>
             <button className='bg-red-500 text-white py-1 px-4 rounded'>
@@ -38,28 +58,32 @@ const CourseRoadMap=()=> {
           id='dropdown'
           className={`${
             isDropdownOpen ? '' : 'hidden'
-          }  flex flex-col gap-4 lg:p-4 p-2  rounde-lg m-2`}
+          }  flex flex-col gap-4 rounde-lg pl-5`}
         >
           <ul
             className='flex flex-col gap-2 lg:p-4 p-2  rounde-lg'
             aria-labelledby='dropdownDefaultButton'
           >
-            <li className='flex items-center justify-between w-full p-2 lg:rounded-full md:rounded-full hover:bg-gray-100 cursor-pointer border-2 rounded-lg'>
-              <a
-                href='/'
-                className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-              >
-                Lộ trình 1
-              </a>
-              <div className="flex gap-3">
-                <button className='bg-yellow-500 text-white py-1 px-4 rounded'>
-                  Sửa
-                </button>
-                <button className='bg-red-500 text-white py-1 px-4 rounded'>
-                  Xóa
-                </button>
-              </div>
-            </li>
+            {
+              listDocument.map((item,index)=>(
+                <li key={index} className='flex items-center bg-white justify-between w-full p-2 lg:rounded-full md:rounded-full hover:bg-gray-100 cursor-pointer border-2 rounded-lg'>
+                  <a
+                    href='/'
+                    className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-black'
+                  >
+                    {item.name}
+                  </a>
+                  <div className="flex gap-3">
+                    <button className='bg-yellow-500 text-white py-1 px-4 rounded'>
+                      Sửa
+                    </button>
+                    <button className='bg-red-500 text-white py-1 px-4 rounded'>
+                      Xóa
+                    </button>
+                  </div>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </div>
